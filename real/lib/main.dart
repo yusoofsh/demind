1c9f0871eb74879b3f34150231fce5c16f20455c
Yusoof Moh <fuzuyx@gmail.com>
2021-04-17 16:19:06 +0700

    feat(real): card with container

--- real/lib/main.dart
+++ real/lib/main.dart
@@ -19,8 +19,20 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
+        ],
       ),
     );
   }
